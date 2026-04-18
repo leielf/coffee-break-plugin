9f707a79b611f72b7cec74b87c9195cce4836208
Leila Babayeva <babaylei@fel.cvut.cz>
2026-04-18 10:49:26 +0200

    dividing getResponse in 2 methods, depending on the model

--- src/main/java/org/example/coffeebreakplugin/AIClient.java
+++ src/main/java/org/example/coffeebreakplugin/AIClient.java
@@ -6,15 +6,26 @@ import java.net.http.HttpResponse;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 import java.util.Map;
 
 public class AIClient {
 
-//    private static String apiKey = "";
-
     public static String getBreakMessage(int edits, long minutes) {
-        try {
-            String prompt = """
+        CoffeeBreakSettings settings = CoffeeBreakSettings.getInstance();
+        // Get the API key from the settings
+        String apiKey = SecureStorage.getApiKey();
+//        String apiKey = settings.getState().apiKey;
+        String ollamaModel = settings.getFirstAvailableModel();
+        boolean isOllama = true;
+        String model = ollamaModel;
+        // Check if API key is present
+        if (apiKey != null && !apiKey.trim().isEmpty()) {
+            isOllama = false;
+            model = apiKey;
+        }
+        String prompt = """
                 You are a friendly assistant inside a coding IDE.
 
                 A developer is taking a break trigger.
@@ -23,13 +34,21 @@ public class AIClient {
                 - minutes coding: %d
                 - edits: %d
 
-                Generate a short, extremely fun coffee break message (maximum 1 sentence). A punch line is essential!
+                Generate a short, extremely fun coffee break message (maximum 1 sentence). A punch line is essential! Also add a suggestion like 'do 10 squats', 'do a moonwalk', etc. 
                 """.formatted(minutes, edits);
+        if(isOllama){
+            System.out.println("trying Ollama, model: " + model);
+            return getOllamaResponse(prompt, model);
+        }
+        System.out.println("trying Hugging face");
+        return getHuggingFaceResponse(apiKey, prompt);
+    }
 
+    private static String getOllamaResponse(String prompt, String ollama){
+        try {
             ObjectMapper mapper = new ObjectMapper();
-
             String body = mapper.writeValueAsString(Map.of(
-                    "model", "codellama",
+                    "model", ollama,
                     "prompt", prompt,
                     "stream", false
             ));
@@ -39,18 +58,6 @@ public class AIClient {
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(body))
                     .build();
-
-//            String body = mapper.writeValueAsString(Map.of(
-//                    "model", "gpt-4.1-mini",
-//                    "input", prompt
-//            ));
-
-//            HttpRequest request = HttpRequest.newBuilder()
-//                    .uri(URI.create("https://api.openai.com/v1/responses"))
-//                    .header("Authorization", "Bearer " + API_KEY)
-//                    .header("Content-Type", "application/json")
-//                    .POST(HttpRequest.BodyPublishers.ofString(body))
-//                    .build();
 
             HttpClient client = HttpClient.newHttpClient();
 
@@ -71,11 +78,40 @@ public class AIClient {
 
             return textNode.asText();
 
-//            return response.body();
-
         } catch (Exception e) {
             e.printStackTrace();
             return "Take a short break! Go and grab a cuppa coffee, do 10 squats or a moonwalk to stretch it out!";
+        }
+    }
+
+    public static String getHuggingFaceResponse(String apiKey, String prompt) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String body = mapper.writeValueAsString(Map.of(
+                    "model", "meta-llama/Llama-3.1-8B-Instruct:cerebras",
+                    "messages", List.of(Map.of("role", "user", "content", prompt)),
+                    "max_tokens", 100
+            ));
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://router.huggingface.co/v1/chat/completions"))
+                    .header("Authorization", "Bearer " + apiKey)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(body))
+                    .build();
+
+            HttpResponse<String> response =
+                    HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("HF RAW: " + response.body());
+
+            JsonNode root = mapper.readTree(response.body());
+            return root.get("choices").get(0).get("message").get("content").asText().replaceAll("^\"|\"$", "");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "☕ Take a break!";
         }
     }
 }
