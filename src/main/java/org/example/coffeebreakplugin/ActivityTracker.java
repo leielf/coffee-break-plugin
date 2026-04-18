cb445594bdf1d155d864400af0d0bbe4cfdbb3fc
Leila Babayeva <babaylei@fel.cvut.cz>
2026-04-18 12:36:52 +0200

    adding edits increment + comments

--- src/main/java/org/example/coffeebreakplugin/ActivityTracker.java
+++ src/main/java/org/example/coffeebreakplugin/ActivityTracker.java
@@ -1,5 +1,12 @@
 package org.example.coffeebreakplugin;
 
+/**
+ * Tracks user activity during an editing session.
+ *
+ * The `ActivityTracker` monitors the number of edits performed and the start time of
+ * the current session. This information is used for determining when to remind the user
+ * to take a break. The tracker is reset periodically to start a new activity session.
+ */
 public class ActivityTracker {
     public static int edits = 0;
     public static long startTime = System.currentTimeMillis();
@@ -7,5 +14,10 @@ public class ActivityTracker {
     public static void reset() {
         edits = 0;
         startTime = System.currentTimeMillis();
+    }
+
+    public static void incrementEdits() {
+        edits++;
+        System.out.println("New edit! edits: " + edits);
     }
 }
